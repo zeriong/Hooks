b85e7cb8b9379f86d3b25960683aa0e7f522ac6b
zeriong <jaeryong95@gmail.com>
2022-10-25 22:58:12 +0900

    Add Hook!

--- src/usePreventLeave.js
+++ src/usePreventLeave.js
@@ -1,4 +1,4 @@
-const usePreventLeave = () => {
+export const usePreventLeave = () => {
     const listener = (event) => {
         event.preventDefault(); //이 후에 받을 이벤트만 단독으로 받기 위해서 우선적으로 프리벤트 디폴트를 한다. 글고 어차피 기본기능 안쓸 듯 리액트니까
         event.returnValue = ""; //없으면 beforeunload 가 실행되지 않는다. 둘이 한 세트임.
