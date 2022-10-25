d18e8e0a81abb154f8c0a1874e07931dc3f03436
zeriong <jaeryong95@gmail.com>
2022-10-25 23:31:17 +0900

    Add Hook!

--- src/useConfirm.js
+++ src/useConfirm.js
@@ -9,8 +9,20 @@ export const useConfirm = (message, onConfirm, onCancel) => { //온캔슬은 취
         if (confirm(message)){
             onConfirm();
         } else{
-            onCancel(); //else 리젝션은 취소했을때 function이기에 필없음 안써두 댐
+            onCancel(); //마찬가지로 취소했을때 필요한 경우만 써두자
         }
     }
     return confirmAction;
 }
+/*
+* const deleteWorld = ()=> console.log("Deleting the world...");
+* const safeWorld = ()=> console.log("You safe the world!!!");
+* const confirmDelete = useConfirm("Are you sure?", deleteWorld, safeWorld);
+*
+* <button onClick={confirmDelete}> Delete World </button>
+*
+* 버튼 클릭시 confirm창이 뜨고 내용은 "Are you sure?"이다
+* 확인을 누르면 deleteWorld
+* 취소누르면 safeWorld가 실행된다.
+* useConfirm만든 argument대로 호출된다. (확인창 메시지내용,확인시 함수, 취소시 함수.)
+*/
