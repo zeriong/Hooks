2ffca523c17ffa957d36c0973c464990c75e12b9
zeriong <jaeryong95@gmail.com>
2022-10-26 22:38:31 +0900

    Add Hook!

--- src/useTitle.js
+++ src/useTitle.js
@@ -9,3 +9,10 @@ export const useTitle = initialTitle => {
     useEffect(updateTitle, [title]);
     return setTitle;
 }
+/*
+* const titleUpdater = useTitle("loading...");
+* 타이틀의 디폴트를 이걸로 바꿔 줄 수 있다. loading...
+*
+* setTimeout(()=> titleUpdater("Home"),5000);
+* 이걸 추가하면 5초 후에 타이틀이 loading... => Home 으로 바뀐다.
+*/
