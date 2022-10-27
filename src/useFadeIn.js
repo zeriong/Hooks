e83dadf61e3c366927a53e6b5f2ba0b5442cd51c
zeriong <jaeryong95@gmail.com>
2022-10-28 00:43:35 +0900

    Add Hook!

--- src/useFadeIn.js
+++ src/useFadeIn.js
@@ -1,24 +1,23 @@
-const useFadeIn = (duration = 1) => {
-    if (typeof duration !== "number") {
+export const useFadeIn = (duration = 1, delay = 0) => {
+    if (typeof duration !== "number" || typeof delay !== "number") {
         return;
     }
     const element = useRef();
     useEffect(() => {
         if (element.current) {
             const { current } = element;
-            current.style.transition = `opacity ${duration}s`;
+            current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
             current.style.opacity = 1;
         }
     }, []);
     return { ref: element, style: { opacity: 0 } };
 };
-
-const fadeInH1 = useFadeIn();
-const fadeInP = useFadeIn(3);
-
-return (
-    <div className="App">
-        <h1 {...fadeInH1}>hello</h1>
-        <p {...fadeInP}>holly sheet</p>
-    </div>
-);
+/*
+* const fadeInH1 = useFadeIn(1, 2);
+* const fadeInP = useFadeIn(5, 10);
+*
+* <h1 {...fadeInH1}>hello</h1>
+* <p {...fadeInP}>holly sheet</p>
+*
+*
+*/
